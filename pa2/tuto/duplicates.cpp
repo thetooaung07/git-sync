a62248203ee1419270ce3e7c489714960635e58c
thetooaung07 <thetooaung07@gmail.com>
2024-03-29 18:45:07 +0100

    uni up

--- pa2/tuto/duplicates.cpp
+++ pa2/tuto/duplicates.cpp
@@ -1,3 +1,26 @@
 //
 // Created by aungthet on 3/28/24.
 //
+
+
+#include "vector"
+
+void func (std::vector<int> &vec) {
+    std::set<int >s (vec.begin(), vec.end());
+    vec = std::vector<int> (s.begin(), s.end());
+
+
+
+
+}
+
+
+int main () {
+
+
+    Hashstring hs;
+
+    hs("abc");
+
+    return 0;
+}
